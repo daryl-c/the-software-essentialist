cacd18bda5a5631facb30f9ea412315c120d59f2
Daryl Carr <darylcarr@live.co.uk>
2023-08-14 00:20:28 +0300

    Wire up TypeORM with user model

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/index.ts
@@ -1,10 +1,24 @@
 import 'dotenv';
 import Express from 'express';
 
+import { User } from "./models/User"
+import { myDataSource } from "./app-data-source"
+
+// establish database connection
+myDataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err: unknown) => {
+        console.error("Error during Data Source initialization:", err)
+    })
+
 const app = Express();
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get('/', async (req, res) => {
+    const users = await myDataSource.getRepository(User).find()
+    res.json(users)
 });
 
 app.listen(3000, () => {
